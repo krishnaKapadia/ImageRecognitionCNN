1af8a5747a0f6023c7aca83bc853def39c7141c9
Krishna Kapadia <kapadikris@myvuw.ac.nz>
2018-10-26 13:24:21 +1300

    Update train.py

--- train.py
+++ train.py
@@ -1,62 +1,40 @@
 #!/usr/bin/env python
 
-"""Description:
-The train.py is to build your CNN model, train the model, and save it for later evaluation(marking)
-This is just a simple template, you feel free to change it according to your own style.
-However, you must make sure:
-1. Your own model is saved to the directory "model" and named as "model.h5"
-2. The "test.py" must work properly with your model, this will be used by tutors for marking.
-3. If you have added any extra pre-processing steps, please make sure you also implement them in "test.py" so that they can later be applied to test images.
-
-©2018 Created by Yiming Peng and Bing Xue
-"""
-from keras import optimizers
-from keras.layers import ZeroPadding2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense
-from tensorflow.keras import backend as K
-from parser import __loader__
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
-
-
 import keras as k
 import tensorflow as tf
 import random
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# Runs the op.
-print(sess.run(c))
-
 # Set random seeds to ensure the reproducible results
 from keras.layers import Conv3D, MaxPooling3D, Convolution2D, Activation, MaxPooling2D, Flatten, \
     Dropout
 from keras.optimizers import Adam
+from sklearn.metrics import classification_report, confusion_matrix
 
-from test import load_images, convert_img_to_array
+from google.colab import drive
+
+drive.mount('/content/drive')
+
+import tensorflow as tf
+print(tf.test.gpu_device_name())
 
 SEED = 309
 np.random.seed(SEED)
 random.seed(SEED)
 tf.set_random_seed(SEED)
 
-scaled_img_dimensions = (224, 224)
+scaled_img_dimensions = [64, 64]
+epochs = 4
 
-# training_cherry = __loader__('data/Train_data/cherry')
-
-
+def construct_perceptron():
+    model.add(Flatten())
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(60, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+  
 def construct_model():
     """
     Construct the CNN model.
@@ -69,92 +47,20 @@ def construct_model():
     ***
     :return: model: the initial CNN model
     """
-    # vgg = k.applications.vgg16.VGG16()
-    # # vgg = k.applications.resnet50.ResNet50()
-    # # print(type(vgg))
-    # model = Sequential()
-    # for layer in vgg.layers:
-    #     print(type(layer))
-    #     model.add(layer)
-    # model.layers.pop()
-    #
-    # for layer in model.layers:
-    #     layer.trainable = False
-    #
-    # model.add(Dense(3, activation='softmax'))
-
-    # Convolution Filters
-    # model.add(Dense(units=64, activation='relu', input_dim=100))
-    # model.add(Dense(units=10, activation='softmax'))
-    # model.add(Conv3D(30, 10, input_shape=(input_img_width, input_img_height, 3)))
-    # model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-
-    # model.add(Convolution2D(32, 3, input_shape=(200, 200, 3)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-    # print('1')
-    # model.add(Convolution2D(32, 3))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-    # print('2')
-    # model.add(Convolution2D(64, 3))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-    # print('3')
-    # # Dropout layer o stop over-fitting
-    # model.add(Flatten())
-
-    # model.add(Dense(1000, activation='relu'))
-    # model.add(Dropout(0.5))
-    #
-    # model.add(Dense(1000, activation='relu'))
-    # model.add(Dropout(0.5))
-    #
-    # model.add(Dense(3))
-    # model.add(Activation('softmax'))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(1))
-    # model.add(Activation('sigmoid'))
-
-
     adam = Adam(lr=0.001)
 
     model = Sequential()
+    model.add(Convolution2D(16, 5, 5, activation='relu', input_shape=(scaled_img_dimensions[0], scaled_img_dimensions[1], 3)))
+    model.add(MaxPooling2D(2, 2))
 
-    # model.add(Convolution2D(64, 11, 4, activation='relu', input_shape=(224, 224, 3)))
-    # model.add(MaxPooling2D(3, 2))
-    # model.add(Convolution2D(192, 5, 1, activation='relu'))
-    # model.add(MaxPooling2D(3, 2))
-    # model.add(Convolution2D(384, (3, 3), strides=1, padding='same', activation='relu'))
-    # model.add(Convolution2D(256, (3, 3), strides=1, padding='same', activation='relu'))
-    # model.add(Convolution2D(256, (3, 3), strides=1, padding='same', activation='relu'))
-    # model.add(MaxPooling2D(3, 2))
-    # model.add(Flatten())
-    # model.add(Dense(2048, activation='relu'))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(2048, activation='relu'))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(3, activation='relu'))
+    model.add(Convolution2D(32, 5, 5, activation='relu'))
+    model.add(MaxPooling2D(2, 2))
 
-    model.add(Convolution2D(20, 5, 5, border_mode="same",input_shape=(224, 224, 3)))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
-    # second set of CONV => RELU => POOL
-    model.add(Convolution2D(50, 5, 5, border_mode="same"))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
-    # set of FC => RELU layers
     model.add(Flatten())
-    model.add(Dense(500))
-    model.add(Activation("relu"))
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
 
-    # softmax classifier
-    model.add(Dense(3))
-    model.add(Activation("softmax"))
-
-    model.compile(loss='categorical_crossentropy',
+    model.compile(loss='binary_crossentropy',
               optimizer=adam,
               metrics=['accuracy'])
     print(model.summary())
@@ -187,30 +93,20 @@ def train_model(model):
     # Scale the image down to 90x90 & Apply random transform,
     datagenerator = ImageDataGenerator(
         # featurewise_center=True, featurewise_std_normalization=True,
-                                       rotation_range=360, shear_range=0.4,
+                                       rotation_range=360, shear_range=0.6,
                                        horizontal_flip=True, vertical_flip=True,
-                                       zoom_range=0.4, rescale=0.4,
-                                       preprocessing_function=preprocess_data
+                                       zoom_range=0.6, rescale=1./255,
+                                       # preprocessing_function=preprocess_data
     )
-
-    train_batches = datagenerator.flow_from_directory('data/Train_data', target_size=scaled_img_dimensions, classes=['cherry', 'strawberry', 'tomato'], batch_size=500)
-
-    imgs, labels = next(train_batches)
-    plot(imgs, titles=labels)
-
+    
+    train_path = '/content/drive/My Drive/Colab Notebooks/Data/Train_data'
+    test_path = '/content/drive/My Drive/Colab Notebooks/Data/Test_data'
+    
+    train_batches = datagenerator.flow_from_directory(train_path, target_size=scaled_img_dimensions, classes=['cherry', 'strawberry', 'tomato'], batch_size=32, class_mode='categorical')
+    
     # Training
     # model.fit(generated_imgs, train_labels[0], epochs=20, batch_size=2000)
-    model.fit_generator(train_batches, shuffle=True, epochs=8, steps_per_epoch=35)
-
-    # Testing
-    test_batches = datagenerator.flow_from_directory('data/Test_data', target_size=scaled_img_dimensions, classes=['cherry', 'strawberry', 'tomato'], batch_size=15)
-
-    test_imgs, test_labels = next(test_batches)
-    plot(test_imgs, titles=test_labels)
-
-    predictions = model.predict_generator(test_batches, steps=1)
-
-    print(predictions)
+    model.fit_generator(train_batches, shuffle=True, epochs=2, steps_per_epoch=4500) 
 
     return model
 
@@ -225,7 +121,9 @@ def save_model(model):
     #   Please remove the comment to enable model save.
     #   However, it will overwrite the baseline model we provided.
     # ***
-    # model.save("model/model.h5")
+    train_path = '/content/drive/My Drive/Colab Notebooks/Data/Train_data'
+#     model.save("model/model.h5")
+    model.save(train_path + "/model.h5")
     print("Model Saved Successfully.")
 
 
